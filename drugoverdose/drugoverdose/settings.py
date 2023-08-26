2fb33a248a697293c36ce42036688821ecfe73c0
Roysav <roysav123@gmail.com>
2023-08-26 13:42:25 +0300

    removed DATABASES['default']['NAME'] in settings

--- drugoverdose/drugoverdose/settings.py
+++ drugoverdose/drugoverdose/settings.py
@@ -76,7 +76,6 @@ WSGI_APPLICATION = "drugoverdose.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
         "USER": os.environ['POSTGRES_USER'],
         "PASSWORD": os.environ['POSTGRES_PASSWORD'],
         "HOST": os.environ['POSTGRES_HOST'],
