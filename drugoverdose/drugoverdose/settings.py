0a1b8c6004171b7bfd55a82d3e4847e995668678
Roysav <roysav123@gmail.com>
2023-08-26 13:43:09 +0300

    added settings.DATABASE['default']['NAME']

--- drugoverdose/drugoverdose/settings.py
+++ drugoverdose/drugoverdose/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = "drugoverdose.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ['POSTGRES_DB'],
         "USER": os.environ['POSTGRES_USER'],
         "PASSWORD": os.environ['POSTGRES_PASSWORD'],
         "HOST": os.environ['POSTGRES_HOST'],
