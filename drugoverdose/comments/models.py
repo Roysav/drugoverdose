5053326539558bd887c4f23317f9684959adc2ae
Roysav <roysav123@gmail.com>
2023-08-26 14:24:22 +0300

    added comments.Comment model

--- drugoverdose/comments/models.py
+++ drugoverdose/comments/models.py
@@ -1,3 +1,11 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    post = models.ForeignKey('posts.Post', related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField(max_length=255)
+    author = models.ForeignKey('auth.User', related_name='comments', on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
