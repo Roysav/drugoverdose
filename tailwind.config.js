9b4eb57c2f9d5571268a32d17b211d344893bb84
Roysav <roysav123@gmail.com>
2023-08-26 17:19:48 +0300

    added tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./drugoverdose/**/templates/**.{html,css,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
