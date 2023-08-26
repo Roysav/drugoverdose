4fc4db894a2f6e5563e6b6c1335b94cf3927f6e4
Roysav <roysav123@gmail.com>
2023-08-26 15:57:23 +0300

    moved Comment model to posts app and deleted comments app

--- drugoverdose/posts/models.py
+++ drugoverdose/posts/models.py
@@ -21,3 +21,9 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', related_name='posts', on_delete=models.SET_NULL, null=True)
 
 
+class Comment(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    post = models.ForeignKey('posts.Post', related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField(max_length=255)
+    author = models.ForeignKey('auth.User', related_name='comments', on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
