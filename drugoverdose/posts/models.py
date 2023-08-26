d0f8be10ddca4b8d9de7a932d12e45d9fa5012d3
Roysav <roysav123@gmail.com>
2023-08-26 13:48:09 +0300

    made migrations for posts.Post model

--- drugoverdose/posts/models.py
+++ drugoverdose/posts/models.py
@@ -4,14 +4,14 @@ import uuid
 from django.db import models
 
 
-class PostType(models.Choices):
-    QUESTION = enum.auto()
-    ANSWER = enum.auto()
+class PostType(models.TextChoices):
+    QUESTION = 'question', 'Question'
+    ANSWER = 'answer', 'Answer'
 
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    type = models.CharField(max_length=20, choices=PostType)
+    type = models.CharField(max_length=20, choices=PostType.choices)
     title = models.CharField(max_length=200, null=True, default=None)
     body = models.TextField(null=True, default=None)
     parent = models.ForeignKey('Post', related_name='children', on_delete=models.CASCADE)
