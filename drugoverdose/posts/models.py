e105e36605fe4f96420edc829ef0d609477ac742
Roysav <roysav123@gmail.com>
2023-08-26 13:41:02 +0300

    added `Post` model and `PostType` enum for `Post.type` choices

--- drugoverdose/posts/models.py
+++ drugoverdose/posts/models.py
@@ -1,3 +1,20 @@
+import enum
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class PostType(models.Choices):
+    QUESTION = enum.auto()
+    ANSWER = enum.auto()
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(max_length=20, choices=PostType)
+    title = models.CharField(max_length=200, null=True, default=None)
+    body = models.TextField(null=True, default=None)
+    parent = models.ForeignKey('Post', related_name='children', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, default=None)
+
